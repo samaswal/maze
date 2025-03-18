51288898b2d272df3738ac52abdce5e8fedec956
Samosval <151852709+samaswal@users.noreply.github.com>
2025-03-18 14:55:17 +0300

    Delete cave directory

--- cave/cave_generation.h
+++
@@ -1,9 +0,0 @@
-#ifndef CAVE_GENERATION_
-#define CAVE_GENERATION_
-#include "../structs.h"
-#include <sys/time.h>
-
-void generate_cave(CaveInfo *c_info);
-void generate_automatic(CaveInfo *c_info);
-
-#endif
